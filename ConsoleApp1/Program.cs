fb483751e555ff2319e7393b2ad3ae8da880fca3
Paul Gibson <paul.gibson2074@gmail.com>
2025-06-22 16:29:53 +1000

    Added c# project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+partial class Program
+{
+    static int Add(int i, int j)
+{
+    return i+j;
+}
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello " + Add(51, 19));
+    }
+}
